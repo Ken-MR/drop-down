c35deb7f3d80838e212eff6dc2ef74133f3d2cad
KenMR <kmr5591@gmail.com>
2023-02-14 22:28:48 -0500

    Create function to toggle list visibility while hovering over list title or child element

--- drop-down.js
+++ drop-down.js
@@ -9,9 +9,21 @@ const menuDropDown = (() => {
       }
     });
   }
-  return {viewOnClick};
+  function viewOnHover (list) {
+    list.addEventListener('mouseover', () => {
+      list.classList.remove('invisible-list');
+    });
+    list.addEventListener('mouseout', () => {
+      list.classList.add('invisible-list');
+    });
+  }
+  return {viewOnClick, viewOnHover};
 })();
 
 const numbersList = document.getElementById('numbers');
 
 menuDropDown.viewOnClick(numbersList);
+
+const lettersList = document.getElementById('letters');
+
+menuDropDown.viewOnHover(lettersList);
